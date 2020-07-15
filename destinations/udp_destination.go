bbf4b9b85db5712018a475679c431c3f0ffa9428
Taranpreet Saini <taranpreet.s16@gmail.com>
2020-07-14 23:28:18 -0400

    udp destination added

--- destinations/udp_destination.go
+++ destinations/udp_destination.go
@@ -1,2 +1,37 @@
 package destinations
 
+import (
+	"fmt"
+	"net"
+	"sync"
+)
+
+// UDPDestination a udp destination
+type UDPDestination struct {
+	Host       string
+	Port       string
+	m          sync.Mutex
+	connection net.Conn
+}
+
+// NewUDPDestination initializes the udp destination
+func NewUDPDestination(host, port string) (*UDPDestination, error) {
+	conn, err := net.Dial("udp", fmt.Sprintf("%v:%v", host, port))
+	if err != nil {
+		return nil, err
+	}
+	return &UDPDestination{host, port, sync.Mutex{}, conn}, nil
+}
+
+// Send to the tcp destination
+func (t *UDPDestination) Send(msg string) error {
+	t.m.Lock()
+	_, err := fmt.Fprintf(t.connection, msg+"\n")
+	t.m.Unlock()
+	return err
+}
+
+// Close the tcp destination
+func (t *UDPDestination) Close() error {
+	return t.connection.Close()
+}
